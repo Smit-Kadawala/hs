bf9575b2b3a699de21e16a411717cfb45a4b4a72
SmitKadawala <60181260+SmitKadawala@users.noreply.github.com>
2022-12-20 21:19:04 +0530

    getData with reactRedux

--- src/redux/reducer/reducers.js
+++ src/redux/reducer/reducers.js
@@ -7,7 +7,7 @@ const initialState = {
 function userReducer(state = initialState, action) {
   switch (action.type) {
     case GET_USER:
-      return {...state, cities: action.payload};
+      return {...state, user: action.payload};
     default:
       return state;
   }
--- src/screens/LoginScreen/LoginScreen.js
+++ src/screens/LoginScreen/LoginScreen.js
@@ -2,26 +2,14 @@ import {StyleSheet, View, FlatList, Text} from 'react-native';
 import React, {useEffect, useState} from 'react';
 import UserInput from '../../Assets/utils/components/UserInput';
 import SubmitButton from '../../Assets/utils/components/SubmitButton';
-import {getUser} from '../../redux/action/Action';
 import {useSelector, useDispatch} from 'react-redux';
+import {getUser} from '../../redux/action/Action';
 
 const LoginScreen = () => {
   const [userEmail, setUserEmail] = useState('');
   const [userPassword, setUserPassword] = useState('');
-  const [data, setData] = useState([]);
   const {user} = useSelector(state => state.userReducer);
   const dispatch = useDispatch();
-
-  // const getUser = async () => {
-  //   try {
-  //     const response = await fetch('http://192.168.0.108:8080/api/user');
-  //     const json = await response.json();
-  //     console.log(json);
-  //     setData(json.movies);
-  //   } catch (error) {
-  //     console.error(error);
-  //   }
-  // };
 
   const loginSubmit = () => {
     if (userEmail.length <= 0 || userPassword.length <= 0) {
@@ -35,35 +23,29 @@ const LoginScreen = () => {
   }, []);
 
   return (
-    // <View>
-    //   <UserInput
-    //     style={styles.UserInputStyle}
-    //     keyboardType={`email-address`}
-    //     placeholder={`Enter Email Address`}
-    //     value={userEmail}
-    //     onChangeText={value => setUserEmail(value)}
-    //   />
-    //   <UserInput
-    //     style={styles.UserInputStyle}
-    //     keyboardType={`default`}
-    //     placeholder={`Enter Password`}
-    //     value={userPassword}
-    //     onChangeText={value => setUserPassword(value)}
-    //   />
-    //   <SubmitButton onPress={loginSubmit} />
-    // </View>
-    <>
-      <FlatList
-        data={user}
-        keyExtractor={({id}, index) => id}
-        renderItem={({item}) => (
-          <Text>
-            {console.log(item)}
-            {item.title}, {item.releaseYear}
-          </Text>
-        )}
+    <View>
+      <UserInput
+        style={styles.UserInputStyle}
+        keyboardType={`email-address`}
+        placeholder={`Enter Email Address`}
+        value={userEmail}
+        onChangeText={value => setUserEmail(value)}
       />
-    </>
+      <UserInput
+        style={styles.UserInputStyle}
+        keyboardType={`default`}
+        placeholder={`Enter Password`}
+        value={userPassword}
+        onChangeText={value => setUserPassword(value)}
+      />
+      <SubmitButton onPress={loginSubmit} />
+
+      <FlatList
+        data={user.response}
+        keyExtractor={({id}, index) => id}
+        renderItem={({item}) => <Text>{item.name}</Text>}
+      />
+    </View>
   );
 };
 
